ac4ab1e5b1b240f665612d54530952e32343da5d
knixa <44531198+knixa@users.noreply.github.com>
2025-08-13 12:42:04 +0200

    feat: option monad

---
+++ option.fsx
@@ -0,0 +1,24 @@
+﻿open System
+
+let tryParseDateTime (input: string) =
+    match DateTime.TryParse input with
+    | true, result -> Some result
+    | _ -> None
+
+type PersonName =
+    { FirstName: string
+      MiddleName: string option
+      LastName: string }
+
+
+let nullObj: string = null
+let nullPri = Nullable<int>()
+
+let fromNullObj = Option.ofObj nullObj
+let fromNullPri = Option.ofNullable nullPri
+
+let toNullObj = Option.toObj fromNullObj
+let toNullPri = Option.toNullable fromNullPri
+
+let defaultStringVal = Option.defaultValue "-----"
+let resultFP = fromNullObj |> defaultStringVal
