6dc81edfd656ef1e909454cd4cfcc7fb01914a3c
knixa <44531198+knixa@users.noreply.github.com>
2025-08-14 21:03:47 +0200

    feat: explore xunit

---
+++ Fleurning.Console/Customer.fs
@@ -0,0 +1,46 @@
+﻿namespace Fleurning.Console
+
+module Customer =
+    type Customer =
+        { Id: int
+          IsVip: bool
+          Credit: decimal }
+
+    let getPurchases customer =
+        try
+            let purchases =
+                if customer.Id % 2 = 0 then
+                    (customer, 120M)
+                else
+                    (customer, 80M)
+
+            Ok purchases
+        with ex ->
+            Error ex
+
+    let tryPromoteToVip purchases =
+        let customer, amount = purchases
+
+        if amount > 100M then
+            { customer with IsVip = true }
+        else
+            customer
+
+    let increaseCreditIfVip customer =
+        try
+            let increase = if customer.IsVip then 100M else 0M
+
+            Ok
+                { customer with
+                    Credit = customer.Credit + increase }
+        with ex ->
+            Error ex
+
+    let upgradeCustomerComposed =
+        getPurchases >> Result.map tryPromoteToVip >> Result.bind increaseCreditIfVip
+
+    let upgradeCustomerPiped customer =
+        customer
+        |> getPurchases
+        |> Result.map tryPromoteToVip
+        |> Result.bind increaseCreditIfVip
---
+++ Fleurning.ConsoleTests/CustomerTests.fs
@@ -0,0 +1,56 @@
+﻿namespace CustomerTests
+
+open System
+open Xunit
+open Fleurning.Console.Customer
+
+module ``I can group my tests in a module and run`` =
+
+    let customerVip = { Id = 1; IsVip = true; Credit = 0.0M }
+
+    let customerStd =
+        { Id = 2
+          IsVip = false
+          Credit = 100.0M }
+
+    [<Fact>]
+    let ``Should give vip more credit`` () =
+        let expected =
+            Ok(
+                { Id = 1
+                  IsVip = true
+                  Credit = 100.0M }
+            )
+
+        let actual = upgradeCustomerComposed customerVip
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ```Should make eligible std to vip and increase credit`` () =
+        let expected =
+            Ok(
+                { Id = 2
+                  IsVip = true
+                  Credit = 200.0M }
+            )
+
+        let actual = upgradeCustomerComposed customerStd
+        Assert.Equal(expected, actual)
+
+    [<Fact>]
+    let ``Should not upgrade ineligible customer to vip`` () =
+        let expected =
+            Ok(
+                { Id = 3
+                  IsVip = false
+                  Credit = 50.0M }
+            )
+
+        let actual =
+            upgradeCustomerComposed
+                { customerStd with
+                    Id = 3
+                    Credit = 50.0M }
+
+        Assert.Equal(expected, actual)
+        
