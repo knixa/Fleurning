89223d50a97ee730a80efb623710918fdf8741ed
knixa <44531198+knixa@users.noreply.github.com>
2025-08-15 12:57:55 +0200

    feat: add multiple items to order

--- Fleurning.Orders/Orders.fs
+++ Fleurning.Orders/Orders.fs
@@ -5,13 +5,19 @@ type Item = { ProductId: int; Quantity: int }
 type Order = { Id: int; Items: Item list }
 
 module Domain =
-    let addItem item order =
-        let items =
-            item :: order.Items
-            |> List.groupBy (fun i -> i.ProductId)
-            |> List.map (fun (id, items) ->
-                { ProductId = id
-                  Quantity = items |> List.sumBy (fun i -> i.Quantity) })
-            |> List.sortBy (fun i -> i.ProductId)
+    let recalculate items =
+        items
+        |> List.groupBy (fun i -> i.ProductId)
+        |> List.map (fun (id, items) ->
+            { ProductId = id
+              Quantity = items |> List.sumBy (fun i -> i.Quantity) })
+        |> List.sortBy (fun i -> i.ProductId)
 
+    let addItem item order =
+        let items = item :: order.Items |> recalculate
+        { order with Items = items }
+
+
+    let addItems newItems order =
+        let items = newItems @ order.Items |> recalculate
         { order with Items = items }
