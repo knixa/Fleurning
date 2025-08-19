19412f89e94df482302682c8c0abc0d89a7d5091
knixa <44531198+knixa@users.noreply.github.com>
2025-08-19 12:50:29 +0200

    feat: read from csv file

---
+++ FileIO/Program.fs
@@ -0,0 +1,40 @@
+﻿open System.IO
+
+type Customer =
+    { CustomerId: string
+      Email: string
+      IsEligible: string
+      IsRegistered: string
+      DateRegistered: string
+      Discount: string }
+
+let parseLine (line: string) : Customer option =
+    match line.Split("|") with
+    | [| customerId; email; eligible; registered; dateRegistered; discount |] ->
+        Some
+            { CustomerId = customerId
+              Email = email
+              IsEligible = eligible
+              IsRegistered = registered
+              DateRegistered = dateRegistered
+              Discount = discount }
+    | _ -> None
+
+let parse (data: string seq) =
+    data
+    |> Seq.skip 1
+    |> Seq.map parseLine
+    |> Seq.choose id
+
+let readFile path =
+    try
+        File.ReadLines(path) |> Ok
+    with ex ->
+        Error ex
+
+let import path =
+    match path |> readFile with
+    | Ok data -> data |> parse |> Seq.iter (fun x -> printfn $"%A{x}")
+    | Error ex -> printfn $"Error: %A{ex.Message}"
+
+Path.Combine(__SOURCE_DIRECTORY__, "Resources", "customers.csv") |> import
