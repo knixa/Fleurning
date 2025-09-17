e21a61389a6180dbc99a746fe9ce39bccfcb9501
knixa <44531198+knixa@users.noreply.github.com>
2025-09-17 07:52:35 +0200

    feat: option computation expression

---
+++ Fleurning.ComputationExpressions/OptionDemo.fs
@@ -0,0 +1,31 @@
+﻿namespace ComputationExpression
+
+[<AutoOpen>]
+module Option =
+
+    type OptionBuilder() =
+        // let!
+        member _.Bind(x, f) = Option.bind f x
+        // return
+        member _.Return(x) = Some x
+        // return!
+        member _.ReturnFrom(x) = x
+
+    let option = OptionBuilder()
+
+module OptionDemo =
+    let multiply x y = x * y
+
+    let divide x y = if y = 0 then None else Some(x / y)
+
+    // let calculate x y =
+    //     divide x y
+    //     |> Option.map (fun v -> multiply v x)
+    //     |> Option.bind (fun t -> divide t y)
+
+    let calculate x y =
+        option {
+            let! v = divide x y
+            let t = multiply v x
+            return! divide t y
+        }
---
+++ Fleurning.ComputationExpressions/Program.fs
@@ -0,0 +1,5 @@
+﻿open ComputationExpression.OptionDemo
+
+
+calculate 8 0 |> printf "Calculate 8 0 = %A"
+calculate 8 2 |> printf "Calculate 8 0 = %A"
