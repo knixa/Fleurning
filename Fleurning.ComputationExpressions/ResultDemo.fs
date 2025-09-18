fd08ce872b5a51d9100c1818a0ffa301a9258df9
knixa <44531198+knixa@users.noreply.github.com>
2025-09-18 07:38:13 +0200

    feat: result expression

---
+++ Fleurning.ComputationExpressions/ResultDemo.fs
@@ -0,0 +1,46 @@
+﻿namespace ComputationExpressions
+
+module ResultDemo =
+    open FsToolkit.ErrorHandling
+
+    type Customer =
+        { Id: int
+          IsVip: bool
+          Credit: decimal }
+
+    let getPurchases customer =
+        try
+            let purchases =
+                if customer.Id % 2 = 0 then
+                    (customer, 120M)
+                else
+                    (customer, 80M)
+
+            Ok purchases
+        with ex ->
+            Error ex
+
+    let tryPromoteToVip purchases =
+        let customer, amount = purchases
+
+        if amount > 100M then
+            { customer with IsVip = true }
+        else
+            customer
+
+    let increaseCreditIfVip customer =
+        try
+            let increase = if customer.IsVip then 100M else 50M
+
+            Ok
+                { customer with
+                    Credit = customer.Credit + increase }
+        with ex ->
+            Error ex
+
+    let upgradeCustomer customer =
+        result {
+            let! purchases = getPurchases customer
+            let promoted = tryPromoteToVip purchases
+            return! increaseCreditIfVip promoted
+        }
