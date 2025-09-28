122a1f3da250d4858719f34df1cb3029a3dd4935
knixa <44531198+knixa@users.noreply.github.com>
2025-09-28 09:04:14 +0200

    feat: init giraffe

---
+++ GiraffeExample/Program.fs
@@ -0,0 +1,53 @@
+open System
+open Giraffe
+open Giraffe.EndpointRouting
+open Giraffe.ViewEngine
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+let indexView =
+    html
+        []
+        [ head [] [ title [] [ str "Giraffe Example" ] ]
+          body
+              []
+              [ h1 [] [ str "I |> F#" ]
+                p [ _class "some-css-class"; _id "someId" ] [ str "Hello World from the Giraffe View Engine" ] ] ]
+
+let sayHelloNameHandler (name: string) : HttpHandler =
+    fun (next: HttpFunc) (ctx: HttpContext) -> {| Response = $"Hello {name}, how are you?" |} |> ctx.WriteJsonAsync
+
+let apiRoutes =
+    [ GET
+          [ route "" (json {| Response = "Hello World!!" |})
+            routef "%s" sayHelloNameHandler ] ]
+
+let endpoints =
+    [ GET [ route "/" (htmlView indexView) ]; subRoute "/api" apiRoutes ]
+
+let notFoundHandler = "Not Found" |> text |> RequestErrors.notFound
+
+let configureApp (appBuilder: IApplicationBuilder) =
+    appBuilder.UseRouting().UseGiraffe(endpoints).UseGiraffe(notFoundHandler)
+
+let configureServices (services: IServiceCollection) =
+    services.AddRouting().AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+    configureServices builder.Services
+
+    let app = builder.Build()
+
+    if app.Environment.IsDevelopment() then
+        app.UseDeveloperExceptionPage() |> ignore
+
+    configureApp app
+
+    app.Run()
+
+    0
+// Exit code
